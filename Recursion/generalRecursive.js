272c3fed187a65ec4dddcd9ddd7394a5440f4e1c
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-01-05 23:48:18 +0530

    recursive factorial

--- Recursion/generalRecursive.js
+++ Recursion/generalRecursive.js
@@ -2,3 +2,14 @@ function recursiveFibonacci(n) {
     if(n < 2) return n;
     return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 }
+
+function recursiveFactorial(n) {
+    if (n === 0) {
+      return 1
+    }
+    return n * recursiveFactorial(n - 1)
+  }
+  
+  console.log(recursiveFactorial(0)) // 1
+  console.log(recursiveFactorial(1)) // 1
+  console.log(recursiveFactorial(5)) // 120
