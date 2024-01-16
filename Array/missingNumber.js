a7ecd8f4c2ddde37dd75d3d57aaa97f7f3b4d99f
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-01-16 15:02:08 +0530

    added additional two approached to missing numbers in array

--- Array/missingNumber.js
+++ Array/missingNumber.js
@@ -1,3 +1,4 @@
+// Approach 1:
 function missingNumber(arr, n) {
     let num;
     let sum1 =  (n * (n + 1)) / 2;
@@ -13,4 +14,41 @@ function missingNumber(arr, n) {
 }
 
 const arr = [1, 2, 4, 5];
-console.log('Missing Number is: ', missingNumber(arr, 5));
+console.log('Approach1: ', missingNumber(arr, 5));
+
+//Approach 2: 
+
+function missingNumber2(arr, n) {
+    let xor1 = 0;
+    let xor2 = 0;
+
+    for(let i = 1; i <= n; i++) {
+        xor1  = xor1 ^ i;
+    }
+
+    for(let i = 0; i < n - 1; i++) {
+        xor2 = xor2 ^ arr[i];
+    }
+
+    return xor1 ^ xor2;
+}
+
+console.log('Approach 2: ', missingNumber2(arr, 5));
+
+//Approach 3:
+
+function missingNumber3(arr, n) {
+    let xor1 = 0;
+    let xor2 = 0;
+
+    for(let i = 0; i < n - 1; i++) {
+        xor2 = xor2 ^ arr[i];
+        xor1 = xor1 ^ (i + 1);
+    }
+
+    xor1 = xor1 ^ n;
+
+    return xor1 ^ xor2;
+}
+
+console.log('Approach 3: ', missingNumber3(arr, 5));
