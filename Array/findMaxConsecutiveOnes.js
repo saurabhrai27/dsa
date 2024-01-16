e153893a531036a0915df6516e177e82a7291d71
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-01-16 15:02:34 +0530

    find max consecutive once

---
+++ Array/findMaxConsecutiveOnes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let max = 0;
+    let current = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] === 1){
+            current++;
+        } else {
+            current = 0;
+        }
+        max = Math.max(max, current);
+    }
+    return max;
+};
