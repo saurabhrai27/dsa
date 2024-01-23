937dc9bc04c75c2bbe5b1d40462661950e401bea
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-01-23 23:04:38 +0530

    get longest subarray using hashing

--- Array/getLongestSubArray.js
+++ Array/getLongestSubArray.js
@@ -1,6 +1,39 @@
+// Approach: Using Hashing
 
+function getLongestSubarray1(a, k) {
+    let n = a.length; // size of the array
 
+    let preSumMap = new Map();
+    let sum = 0;
+    let maxLen = 0;
+    for (let i = 0; i < n; i++) {
+        // calculate the prefix sum till index i
+        sum += a[i];
 
+        // if the sum = k, update the maxLen
+        if (sum === k) {
+            maxLen = Math.max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x - k
+        let rem = sum - k;
+
+        // calculate the length and update maxLen
+        if (preSumMap.has(rem)) {
+            let len = i - preSumMap.get(rem);
+            maxLen = Math.max(maxLen, len);
+        }
+
+        // update the map checking the conditions
+        if (!preSumMap.has(sum)) {
+            preSumMap.set(sum, i);
+        }
+    }
+
+    return maxLen;
+}
+
+// Approach: Using 2 pointers
 function getLongestSubarray(a, k) {
     let n = a.length; // size of the array
 
