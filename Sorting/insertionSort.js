3ba3ae82d2ac56f558c4f85d4946f46229992705
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-04-10 00:23:48 +0530

    updated inserstion sort logic

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,12 +1,12 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j = j - 1;
+function insertionSort(n) {
+  for(let i = 0; i < n; i++) {
+    let j = i;
+    while(j > 0 && arr[j - 1] > arr[j]){
+      let temp = arr[j-1];
+      arr[j-1] = arr[j];
+      arr[j] = temp;
+      j--;
     }
-    arr[j + 1] = key;
   }
 }
 
