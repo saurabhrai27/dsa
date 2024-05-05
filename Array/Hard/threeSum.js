856bb98c6caae85bb85367358e7aa1501409f38e
Saurabh Rai <saurabhrai.cec@gmail.com>
2024-05-05 18:14:09 +0530

    Better and Optimal approach for 3 sum

--- Array/Hard/threeSum.js
+++ Array/Hard/threeSum.js
@@ -4,7 +4,11 @@
  */
 
 
-// Brute Force Aproach
+/**
+ * Brute Force Aproach 
+ * Time Complexity : O(N3 * log(no. of unique triplets)), where N = size of the array)
+ * Space Complexity: O(2 * no. of the unique triplets) as we are using a set data structure and a list to store the triplets.
+ */ 
 function tripletBrute(n, arr) {
     let unique = new Set();
     for(let i = 0; i < n; i++) {
@@ -25,4 +29,71 @@ function tripletBrute(n, arr) {
 
 const arr = [-1,0,1,2,-1,-4];
 const n = arr.length;
+console.log('Brute:');
 console.log(tripletBrute(n, arr));
+
+/**
+ * Better Solution
+ * Time Complexity: O(N2 * log(no. of unique triplets)), where N = size of the array.
+ * Space Complexity: O(2 * no. of the unique triplets) + O(N) as we are using a set data structure and a list to store the triplets 
+ * and extra O(N) for storing the array elements in another set.
+ */
+
+function tripletsBetter(n, arr) {
+    let unique = new Set();
+
+    for(let i = 0; i < n; i++) {
+        const hash = new Set();
+        for(let j = i + 1; j < n; j++) {    
+            let third = - (arr[i] + arr[j]);
+            if(hash.has(third)) {
+                let temp = [arr[i], arr[j], third];
+                temp.sort((a,b) => a - b);
+                unique.add(JSON.stringify(temp));
+            }
+            hash.add(arr[j]);
+        }
+    }
+
+    const result = Array.from(unique).map(item => JSON.parse(item));
+    return result;
+}
+
+console.log('Better:');
+console.log(tripletsBetter(n, arr));
+
+/**
+ * Optimal Approach
+ * Time Complexity: O(NlogN)+O(N2), where N = size of the array
+ * Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem. 
+    So, from that perspective, space complexity can be written as O(1).
+ */
+function tripletOptimal(n, arr) {
+    const result = []
+    arr.sort((a, b) => a - b);
+    for(let i = 0; i < n; i++) {
+        if(i !== 0 && arr[i] === arr[i - 1]) continue;
+
+        let j = i + 1;
+        let k = n - 1;
+        while (j < k) {
+            let sum = arr[i] + arr[j] + arr[k];
+            if(sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
+            } else {
+                result.push([arr[i], arr[j], arr[k]]);
+                // skip the duplicates:
+                j++;
+                k--;
+                while (j < k && arr[j] === arr[j - 1]) j++;
+                while (j < k && arr[k] === arr[k + 1]) k--; 
+            }
+        }
+    }
+    return result
+}
+
+console.log('Optimal:');
+console.log(tripletOptimal(n, arr));
